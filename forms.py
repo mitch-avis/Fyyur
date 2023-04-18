e8851bb8f95a7749cf4f83f2ee0e89690ce809f3
mitch-avis <mitchavis@gmail.com>
2023-04-17 22:17:41 -0600

    update forms

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
+import re
 from datetime import datetime
 
-from flask_wtf import Form
+from enums import Genre, State
+from flask_wtf import FlaskForm as Form
 from wtforms import (
     BooleanField,
     DateTimeField,
@@ -20,198 +22,86 @@ class ShowForm(Form):
 class VenueForm(Form):
     name = StringField("name", validators=[DataRequired()])
     city = StringField("city", validators=[DataRequired()])
-    state = SelectField(
-        "state",
-        validators=[DataRequired()],
-        choices=[
-            ("AL", "AL"),
-            ("AK", "AK"),
-            ("AZ", "AZ"),
-            ("AR", "AR"),
-            ("CA", "CA"),
-            ("CO", "CO"),
-            ("CT", "CT"),
-            ("DE", "DE"),
-            ("DC", "DC"),
-            ("FL", "FL"),
-            ("GA", "GA"),
-            ("HI", "HI"),
-            ("ID", "ID"),
-            ("IL", "IL"),
-            ("IN", "IN"),
-            ("IA", "IA"),
-            ("KS", "KS"),
-            ("KY", "KY"),
-            ("LA", "LA"),
-            ("ME", "ME"),
-            ("MT", "MT"),
-            ("NE", "NE"),
-            ("NV", "NV"),
-            ("NH", "NH"),
-            ("NJ", "NJ"),
-            ("NM", "NM"),
-            ("NY", "NY"),
-            ("NC", "NC"),
-            ("ND", "ND"),
-            ("OH", "OH"),
-            ("OK", "OK"),
-            ("OR", "OR"),
-            ("MD", "MD"),
-            ("MA", "MA"),
-            ("MI", "MI"),
-            ("MN", "MN"),
-            ("MS", "MS"),
-            ("MO", "MO"),
-            ("PA", "PA"),
-            ("RI", "RI"),
-            ("SC", "SC"),
-            ("SD", "SD"),
-            ("TN", "TN"),
-            ("TX", "TX"),
-            ("UT", "UT"),
-            ("VT", "VT"),
-            ("VA", "VA"),
-            ("WA", "WA"),
-            ("WV", "WV"),
-            ("WI", "WI"),
-            ("WY", "WY"),
-        ],
-    )
+    state = SelectField("state", validators=[DataRequired()], choices=State.choices())
     address = StringField("address", validators=[DataRequired()])
     phone = StringField("phone")
     image_link = StringField("image_link")
     genres = SelectMultipleField(
-        # TODO implement enum restriction
         "genres",
         validators=[DataRequired()],
-        choices=[
-            ("Alternative", "Alternative"),
-            ("Blues", "Blues"),
-            ("Classical", "Classical"),
-            ("Country", "Country"),
-            ("Electronic", "Electronic"),
-            ("Folk", "Folk"),
-            ("Funk", "Funk"),
-            ("Hip-Hop", "Hip-Hop"),
-            ("Heavy Metal", "Heavy Metal"),
-            ("Instrumental", "Instrumental"),
-            ("Jazz", "Jazz"),
-            ("Musical Theatre", "Musical Theatre"),
-            ("Pop", "Pop"),
-            ("Punk", "Punk"),
-            ("R&B", "R&B"),
-            ("Reggae", "Reggae"),
-            ("Rock n Roll", "Rock n Roll"),
-            ("Soul", "Soul"),
-            ("Other", "Other"),
-        ],
+        choices=Genre.choices(),
     )
     facebook_link = StringField("facebook_link", validators=[URL()])
     website_link = StringField("website_link")
-
     seeking_talent = BooleanField("seeking_talent")
-
     seeking_description = StringField("seeking_description")
+
+    def validate(self, **kwargs):
+        validated = Form.validate(self)
+
+        if not validated:
+            return False
+        if not is_valid_phone(self.phone.data):
+            self.phone.errors.append("Invalid phone number.")
+            return False
+        if not set(self.genres.data).issubset(dict(Genre.choices()).keys()):
+            self.genres.errors.append("Invalid genres.")
+            return False
+        if self.state.data not in dict(State.choices()).keys():
+            self.state.errors.append("Invalid state.")
+            return False
+        return True
 
 
 class ArtistForm(Form):
     name = StringField("name", validators=[DataRequired()])
     city = StringField("city", validators=[DataRequired()])
-    state = SelectField(
-        "state",
-        validators=[DataRequired()],
-        choices=[
-            ("AL", "AL"),
-            ("AK", "AK"),
-            ("AZ", "AZ"),
-            ("AR", "AR"),
-            ("CA", "CA"),
-            ("CO", "CO"),
-            ("CT", "CT"),
-            ("DE", "DE"),
-            ("DC", "DC"),
-            ("FL", "FL"),
-            ("GA", "GA"),
-            ("HI", "HI"),
-            ("ID", "ID"),
-            ("IL", "IL"),
-            ("IN", "IN"),
-            ("IA", "IA"),
-            ("KS", "KS"),
-            ("KY", "KY"),
-            ("LA", "LA"),
-            ("ME", "ME"),
-            ("MT", "MT"),
-            ("NE", "NE"),
-            ("NV", "NV"),
-            ("NH", "NH"),
-            ("NJ", "NJ"),
-            ("NM", "NM"),
-            ("NY", "NY"),
-            ("NC", "NC"),
-            ("ND", "ND"),
-            ("OH", "OH"),
-            ("OK", "OK"),
-            ("OR", "OR"),
-            ("MD", "MD"),
-            ("MA", "MA"),
-            ("MI", "MI"),
-            ("MN", "MN"),
-            ("MS", "MS"),
-            ("MO", "MO"),
-            ("PA", "PA"),
-            ("RI", "RI"),
-            ("SC", "SC"),
-            ("SD", "SD"),
-            ("TN", "TN"),
-            ("TX", "TX"),
-            ("UT", "UT"),
-            ("VT", "VT"),
-            ("VA", "VA"),
-            ("WA", "WA"),
-            ("WV", "WV"),
-            ("WI", "WI"),
-            ("WY", "WY"),
-        ],
-    )
-    phone = StringField(
-        # TODO implement validation logic for state
-        "phone"
-    )
+    state = SelectField("state", validators=[DataRequired()], choices=State.choices())
+    phone = StringField("phone")
     image_link = StringField("image_link")
     genres = SelectMultipleField(
         "genres",
         validators=[DataRequired()],
-        choices=[
-            ("Alternative", "Alternative"),
-            ("Blues", "Blues"),
-            ("Classical", "Classical"),
-            ("Country", "Country"),
-            ("Electronic", "Electronic"),
-            ("Folk", "Folk"),
-            ("Funk", "Funk"),
-            ("Hip-Hop", "Hip-Hop"),
-            ("Heavy Metal", "Heavy Metal"),
-            ("Instrumental", "Instrumental"),
-            ("Jazz", "Jazz"),
-            ("Musical Theatre", "Musical Theatre"),
-            ("Pop", "Pop"),
-            ("Punk", "Punk"),
-            ("R&B", "R&B"),
-            ("Reggae", "Reggae"),
-            ("Rock n Roll", "Rock n Roll"),
-            ("Soul", "Soul"),
-            ("Other", "Other"),
-        ],
+        choices=Genre.choices(),
     )
     facebook_link = StringField(
-        # TODO implement enum restriction
         "facebook_link",
         validators=[URL()],
     )
-
     website_link = StringField("website_link")
-
     seeking_venue = BooleanField("seeking_venue")
-
     seeking_description = StringField("seeking_description")
+
+    def validate(self, **kwargs):
+        validated = Form.validate(self)
+
+        if not validated:
+            return False
+        if not is_valid_phone(self.phone.data):
+            self.phone.errors.append("Invalid phone number.")
+            return False
+        if not set(self.genres.data).issubset(dict(Genre.choices()).keys()):
+            self.genres.errors.append("Invalid genres.")
+            return False
+        if self.state.data not in dict(State.choices()).keys():
+            self.state.errors.append("Invalid state.")
+            return False
+        return True
+
+
+def is_valid_phone(number):
+    """Validate phone numbers like:
+    1234567890 - no space
+    123.456.7890 - dot separator
+    123-456-7890 - dash separator
+    123 456 7890 - space separator
+
+    Patterns:
+    000 = [0-9]{3}
+    0000 = [0-9]{4}
+    -.  = ?[-. ]
+
+    Note: (? = optional) - Learn more: https://regex101.com/
+    """
+    regex = re.compile("^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$")  # noqa: W0605
+    return regex.match(number)
